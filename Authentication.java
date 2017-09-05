7d2cf1f03a5dc7d2ad382a4fd4bc75e3cd4d282b
MuntaqimE <31520592+MuntaqimE@users.noreply.github.com>
2017-09-05 00:16:59 -0400

    Add files via upload

--- Authentication.java
+++ Authentication.java
@@ -1,12 +1,18 @@
+/**
+ * @author Muntaqim Elahi
+ * Purpose: To successfully authorize username and password to display text accordingly
+ * created by me22810 on 09/04/2017
+ */
+
 import java.util.Scanner;
 public class Authentication {
-public static void main (String[] args) {
-String username, password;
-Scanner keyboard = new Scanner(System.in);
-System.out.println("Enter your username");
-username = keyboard.next();
-System.out.println("Enter your password");
-password = keyboard.next();
-System.out.println("Hello" + username + ",Welcome to CSC200 class! and Your password is" + password);
-}
+    public static void main (String[] args) {
+        String username, password;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter your username");
+        username = keyboard.next();
+        System.out.println("Enter your password");
+        password = keyboard.next();
+        System.out.println("Hello" + username + ",Welcome to CSC200 class! and Your password is " + password);
+    }
 }
